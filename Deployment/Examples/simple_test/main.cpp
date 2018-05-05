62efa302ca542f9728a41d8baa034018657d545d
Vincent Pollet <vpollet@corp.logitech.com>
2018-05-05 09:53:55 +0200

    Modif main to retrieve audio data through serial interface

--- Deployment/Examples/simple_test/main.cpp
+++ Deployment/Examples/simple_test/main.cpp
@@ -21,6 +21,67 @@
  */
 
 #include "kws_ds_cnn.h"
+#include "mbed_rpc.h"
+// #include "wav_data.h"
+
+Timer T;
+Serial pc(USBTX, USBRX);
+
+void inference(Arguments *input, Reply *output);
+RPCFunction rpc_inference(&inference, "inference");
+
+
+void inference(Arguments *input, Reply *output)
+{
+  /*
+  // Arguments are already parsed into argv array of char*
+  printf("Object name = %s\r\n",input->obj_name);
+  printf("Method name = %s\r\n",input->method_name);
+  for (int i=0; i < input->argc; i++)
+    printf("argv[%1d] = %s \r\n",i,input->argv[i]);
+  */
+  
+  int16_t audio_buf[16000];
+  for (int i=0; i < input->argc; ++i)
+    audio_buf[i] = (int16_t)input->getArg<int>(); // convert to int and pray it is in range of short int
+  
+  char output_class[12][8] = {"Silence", "Unknown","yes","no","up","down","left","right","on","off","stop","go"};
+  KWS_DS_CNN kws(audio_buf);
+
+  T.start();
+  int start=T.read_us();
+  kws.extract_features(); //extract mfcc features
+  kws.classify();	  //classify using dnn
+  int end=T.read_us();
+  T.stop();
+  int max_ind = kws.get_top_class(kws.output);
+  pc.printf("Total time : %d us\r\n",end-start);
+  pc.printf("Detected %s (%d%%)\r\n",output_class[max_ind],((int)kws.output[max_ind]*100/128));
+  
+  char buffer[2000];
+  sprintf(buffer, "Total time : %d us\r\n",end-start);
+  output->putData(buffer);
+  sprintf(buffer, "Detected %s (%d%%)\r\n",output_class[max_ind],((int)kws.output[max_ind]*100/128));
+  output->putData(buffer);
+}
+
+int main()
+{
+  char buf[4000], outbuf[4000];
+  while(1)
+  {
+    pc.scanf("%1999[^\r\n]", buf);
+    RPC::call(buf, outbuf);
+    pc.printf("%s\r\n", outbuf);
+  }
+
+  return 0;
+}
+
+
+/*
+
+#include "kws_ds_cnn.h"
 #include "wav_data.h"
 
 int16_t audio_buffer[16000]=WAVE_DATA;
@@ -45,3 +106,6 @@ int main()
 
   return 0;
 }
+
+
+*/
